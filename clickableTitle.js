5fa46ec8b2f8f465f54e83f9f486071817c87caa
lyndayouens <53065453+lyndayouens@users.noreply.github.com>
2019-08-08 21:49:24 +1200

    Initial commit

---
+++ clickableTitle.js
@@ -0,0 +1,56 @@
+var clickableTooltip = (function(){
+    var pub = {};
+    var titleEls;
+
+    function showTooltip() {
+        var title = this.querySelectorAll(".clickableTitle");
+        if (!title.length) {
+            hideTooltips();
+            var title = document.createElement("span");
+            title.innerText = this.getAttribute("title");
+            title.classList.add("clickableTitle");
+            var rect = this.getBoundingClientRect();
+            title.style.top = rect.top + this.clientHeight + "px";
+            this.appendChild(title);
+        } else {
+            hideTooltips();
+        }
+    }
+
+    function hideTooltips() {
+        var title = document.querySelectorAll(".clickableTitle");
+        var i;
+        if (title.length) {
+            for (i = 0; i < title.length; i += 1) {
+                title[i].parentNode.removeChild(title[i]);
+            }
+        }
+    }
+
+    pub.dismissAllTooltips = function(event) {
+        if (!event.target.matches(".AppName")) {
+            hideTooltips();
+        }
+    };
+
+
+    pub.setup = function() {
+        titleEls = document.querySelectorAll("[title]");
+        var i;
+        for (i = 0; i < titleEls.length; i+=1) {
+            titleEls[i].addEventListener("click", showTooltip);
+        }
+        document.addEventListener("click", hideTooltips());
+    };
+    return pub;
+})();
+
+if(window.addEventListener) {
+    window.addEventListener("load", clickableTooltip.setup);
+    window.onclick = clickableTooltip.dismissAllTooltips;
+}else if(window.attachEvent) {
+    window.attachEvent("onload", clickableTooltip.setup);
+    window.onclick = clickableTooltip.dismissAllTooltips;
+}else{
+    console.log("Could not attach 'clickableTooltip.setup' to the 'window.onload' event");
+}
