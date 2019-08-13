d8f793ec2134865a37a8f69fcd1466dd69283e26
lyndayouens <53065453+lyndayouens@users.noreply.github.com>
2019-08-14 08:55:36 +1200

    Add files via upload

---
+++ CookBrosPortalCode/clickableTitle.js
@@ -0,0 +1,64 @@
+var clickableTooltip = (function(){
+    var pub = {};
+    var titleEls;
+
+    function showTooltip() {
+        var title = this.querySelectorAll(".clickableTitle");
+        if (!title.length) {
+            pub.hideTooltips();
+            var title = document.createElement("span");
+            title.innerText = this.getAttribute("title");
+            title.classList.add("clickableTitle");
+            var rect = this.getBoundingClientRect();
+            title.style.top = rect.top + this.clientHeight + "px";
+            this.appendChild(title);
+            this.classList.add("selectedClickableTitle");
+        } else {
+            pub.hideTooltips();
+        }
+    }
+
+    pub.hideTooltips = function() {
+        var title = document.querySelectorAll(".clickableTitle");
+        var i;
+        if (title.length) {
+            for (i = 0; i < title.length; i += 1) {
+                title[i].parentNode.removeChild(title[i]);
+            }
+        }
+        var label = document.querySelectorAll(".selectedClickableTitle");
+        var i;
+        if (label.length) {
+            for (i = 0; i < label.length; i += 1) {
+                label[i].classList.remove("selectedClickableTitle");
+            }
+        }
+    }
+
+    pub.dismissAllTooltips = function(event) {
+        if (!event.target.matches(".AppName")) {
+            pub.hideTooltips();
+        }
+    };
+
+
+    pub.setup = function() {
+        titleEls = document.querySelectorAll("[title]");
+        var i;
+        for (i = 0; i < titleEls.length; i+=1) {
+            titleEls[i].addEventListener("click", showTooltip);
+        }
+        document.addEventListener("click", pub.hideTooltips());
+    };
+    return pub;
+})();
+
+if(window.addEventListener) {
+    window.addEventListener("load", clickableTooltip.setup);
+    window.onclick = clickableTooltip.dismissAllTooltips;
+}else if(window.attachEvent) {
+    window.attachEvent("onload", clickableTooltip.setup);
+    window.onclick = clickableTooltip.dismissAllTooltips;
+}else{
+    console.log("Could not attach 'clickableTooltip.setup' to the 'window.onload' event");
+}
